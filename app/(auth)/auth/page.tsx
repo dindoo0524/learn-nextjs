ff388a63a1258aadee6eb59a72634fca09be57be
JinjuNa <dindoo0524@gmail.com>
2024-11-14 22:20:20 +0900

    refactor: file structure

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Auth() {
   return (
     <div className="flex flex-col items-center justify-between min-h-screen p-6">
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
